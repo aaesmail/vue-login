b08f3f9e708f352bc3c8bc93e960f4796ea5f1d9
Ali <aliadel56000@gmail.com>
2021-06-03 03:40:42 +0200

    Made the 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Not Found</h1>
+  <div>
+    <img src="../assets/404@2x.png" alt="Not found" />
+    <h1>404-Page Not Found!</h1>
+    <p>Sorry, that page doesn't exist. What would you like to do?</p>
+  </div>
 </template>
+
+<style lang="sass" scoped>
+@import '../styles/variables'
+
+div
+  height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+img
+  width: 300px
+  @media (min-width: $layout-breakpoint-small)
+    width: 500px
+
+h1
+  color: navy
+  font-size: 25px
+  @media (min-width: $layout-breakpoint-small)
+    font-size: 40px
+
+p
+  color: $primary-color
+</style>
